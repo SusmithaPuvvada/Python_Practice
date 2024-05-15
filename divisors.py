e75ed69752c3940a2b2023dd559c16f6542b6052
SusmithaPuvvada <susmithapuvvada@gmail.com>
2024-05-15 19:11:55 +0530

    Added the program concept

--- divisors.py
+++ divisors.py
@@ -1,3 +1,5 @@
+#Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+
 num = int(input('enter a number:'))
 
 list_range = list(range(1,num+1))
--- list_less_than_ten.py
+++ list_less_than_ten.py
@@ -1,3 +1,5 @@
+#write a program that prints out all the elements of the list that are less than 10.
+
 a=[]
 n = int(input('enter a number:'))
 for i in range (0,n):
